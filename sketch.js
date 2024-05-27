aae784c64df732036e6493ad50e3ecb423ae593f
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-05-27 10:07:45 -0300

    Adicionando Funções mo arq sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,36 @@
+//variáveis da bolinha
 let xBolinha = 300;
 let yBolinha = 200;
 let diametroBolinha = 15;
 let raio = diametroBolinha / 2;
 
+//velocidade da bolinha
 let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 6;
 
 function setup() {
-    createCanvas(800, 400);
+    createCanvas(600, 400);
 }
 
 function draw() {
-    background(0);
+    background(0); //1 - Desenha o background 
+    mostraBolinha(); // 2 - Desenha a bolinha
+    movimentaBolinha(); // 3 - Movimenta a Bolinha
+    verificaColisaoBorda(); // 4 - Verifica Colisão da bolinha
+   
+   // 5- Volta para o início da função draw()
+  }
+  
+function mostraBolinha() {
     circle (xBolinha, yBolinha, diametroBolinha);
+}
+
+function movimentaBolinha() {
     xBolinha += velocidadeXBolinha;
     yBolinha += velocidadeYBolinha;
+}
 
+function verificaColisaoBorda() {
     if (xBolinha + raio > width || xBolinha - raio< 0) {
         velocidadeXBolinha *= -1;//movimenta em sentindo oposto -1
     }
@@ -23,4 +38,3 @@ function draw() {
         velocidadeYBolinha *= -1
     }
 }
-
